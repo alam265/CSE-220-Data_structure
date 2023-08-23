6111eb75b7c12728a7ffcc36650ff1aa5b19f0e5
alam265 <113450350+alam265@users.noreply.github.com>
2023-08-23 21:52:14 +0600

    added

---
+++ Tree/Sum of Their Child/code.py
@@ -0,0 +1,94 @@
+class TreeNode:
+    def __init__(self,val,lc,rc):
+        self.value = val 
+        self.lchild = lc
+        self.rchild = rc 
+
+class Node:
+    def __init__(self,ref,next):
+        self.ref = ref 
+         
+        self.next = next 
+
+class Queue:
+    def __init__(self):
+        self.front = None 
+        self.back = None 
+
+    def enqueue(self,ref):
+        if self.front == None:
+            self.front = Node(ref,None)
+            self.back = self.front 
+        else:
+            newNode = Node(ref,None)
+            self.back.next = newNode
+            self.back = newNode
+
+    def dequeue(self):
+        if self.front == None:
+            return 'Empty Queue'
+        else:
+            x = self.front 
+            self.front = self.front.next 
+            return x.ref 
+        
+    def peek(self):
+        if self.front == None:
+            return 'Queue is Empty'
+        return self.front.elem 
+    
+    def display(self):
+        p = self.front
+        while p!=None:
+            print(p.elem,end=' ')
+            p = p.next 
+    def isEmpty(self):
+        if self.front == None:
+            return True
+        return False
+
+
+def Tree_creation():
+    q = Queue()
+    v = int(input('Enter Root value:'))
+    root = TreeNode(v,None,None)
+    r =root
+    q.enqueue(root)
+
+    while not q.isEmpty():
+        p = q.dequeue()
+        x = int(input(f"Enter Left child of {p.value}: "))
+        if x!=-1:
+            temp = TreeNode(x,None,None)
+            p.lchild = temp 
+            q.enqueue(temp)
+
+        x = int(input(f"Enter Right child of {p.value}: "))
+        if x!=-1:
+            temp = TreeNode(x,None,None)
+            p.rchild = temp 
+            q.enqueue(temp)
+    return r
+
+
+root = Tree_creation()
+
+def SumOfChild(root):
+    if root is  None or (root.lchild is None and root.rchild is None):
+        return True
+    left = 0
+    right = 0
+    if root.lchild != None:
+        left = root.lchild.value
+    if root.rchild != None:
+        right = root.rchild.value 
+
+    if left + right == root.value:
+        return SumOfChild(root.lchild) and SumOfChild(root.rchild)
+    return False 
+ 
+if SumOfChild(root):
+    print("Yes")
+else:
+    print("No")
+
