02e396c13063624ad1c198ec4ee0fba70a5832cb
alam265 <113450350+alam265@users.noreply.github.com>
2023-12-21 18:35:34 +0600

    added

--- exp.py
+++ exp.py
@@ -1,14 +1,15 @@
 def binary_search_first_index_and_count(arr, low, high, key):
-    global count
+    
     while low <= high:
         mid = (low +high) // 2
 
         if arr[mid] == key:
-            count+=1
+          
             # Check if this is the first occurrence of the key
             if mid == low or arr[mid - 1] < key:
-               
-                return mid
+                count = count_occurrences(arr,mid,key)
+                
+                return mid,count
             else:
                 high = mid-1 
                 
@@ -17,10 +18,23 @@ def binary_search_first_index_and_count(arr, low, high, key):
         else:
             low = mid+1 
 
+   
     return 'Nothing found'  # Return the negative index and count 0 if key is not found
 
-count = 0
+def count_occurrences(arr, index, key):
+    count = 1
+    left = index - 1
+    while left >= 0 and arr[left] == key:
+        count += 1
+        left -= 1
+    right = index + 1
+    while right < len(arr) and arr[right] == key:
+        count += 1
+        right += 1
+    return count
+
+
 arr = [2,2,3,3,5,8,9,12,14,17]
-x = binary_search_first_index_and_count(arr,0,len(arr)-1,17)
-print(x)
+idx,count = binary_search_first_index_and_count(arr,0,len(arr)-1,3)
+print(idx)
 print(count)
